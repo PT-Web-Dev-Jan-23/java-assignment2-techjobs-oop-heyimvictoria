a9276b82e60e749ec4c04b00e6df6a3a572a1631
Victoria M <vmorris517@gmail.com>
2023-04-23 23:51:08 -0500

    completed support classes, and started to code Job class

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -17,9 +19,26 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    public String toString(){
+        return value;
+    }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object i) {
+        if (this == i) return true;
+        if (i == null || getClass() != i.getClass()) return false;
+        PositionType that = (PositionType) i;
+        return id == that.id && value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
 
     // Getters and Setters:
 
