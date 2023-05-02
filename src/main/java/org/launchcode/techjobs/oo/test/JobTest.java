e1234da78ba7b7d1e65457796f14e54c0905703d
Victoria M <vmorris517@gmail.com>
2023-05-01 20:30:22 -0500

    trying to test the labels, it passes but fails when all tests run

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -22,7 +22,7 @@ public class JobTest {
 
         @Test
         public void testJobConstructorSetsAllFields(){
-        Job jobsTest = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job jobsTest = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
         //checks that class and value is properly assigned
             assertTrue(jobsTest.getName() instanceof String); //checks that the objects is an instance of the name Sting
             assertTrue(jobsTest.getEmployer() instanceof Employer);
@@ -31,11 +31,11 @@ public class JobTest {
             assertTrue(jobsTest.getCoreCompetency() instanceof CoreCompetency);
 
             //checks each field is properly assigned...chatGPT is awesome
-            assertEquals("Product tester", jobsTest.getName());
-            assertEquals("ACME", jobsTest.getEmployer().getValue());
-            assertEquals("Desert", jobsTest.getLocation().getValue());
-            assertEquals("Quality control", jobsTest.getPositionType().getValue());
-            assertEquals("Persistence", jobsTest.getCoreCompetency().getValue());
+            assertEquals("Web Developer", jobsTest.getName());
+            assertEquals("LaunchCode", jobsTest.getEmployer().getValue());
+            assertEquals("St. Louis", jobsTest.getLocation().getValue());
+            assertEquals("Front-end developer", jobsTest.getPositionType().getValue());
+            assertEquals("JavaScript", jobsTest.getCoreCompetency().getValue());
 
         }
         @Test
@@ -49,16 +49,29 @@ public class JobTest {
 
         @Test
     public void testToStringStartsAndEndsWithNewLine(){
-            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job job1 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
             String expectedData = "\nID: 1\n" +
-                    "\nName: Product tester\n" +
-                    "\nEmployer: ACME\n" +
-                    "\nLocation: Desert\n" +
-                    "\nPosition Type: Quality control\n" +
-                    "\nCompetency: Persistence\n\n";
+                    "\nName: Web Developer\n" +
+                    "\nEmployer: LaunchCode\n" +
+                    "\nLocation: St. Louis" +
+                    "\nPosition Type: Front-end developer\n" +
+                    "\nCompetency: JavaScript\n\n";
             assertEquals(expectedData.charAt(0), job1.toString().charAt(0));
             //second assertEquals checks for the last character newline
-//            assertEquals(expectedData.charAt(expectedData.length()-1), job1.toString().charAt(job1.toString().length()-1));
+            assertEquals(expectedData.charAt(expectedData.length()-1), job1.toString().charAt(job1.toString().length()-1));
         }
-
+        @Test
+        public void testToStringContainsCorrectLabelsAndData(){
+            Job job = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+            String jobString = job.toString();
+            //created a job object and called the toString method to get the string representation of the object.
+            // ..may refactor this seems like a lot but it passes
+            assertTrue(jobString.contains("ID: 1"));
+            assertTrue(jobString.contains("Name: Web Developer"));
+            assertTrue(jobString.contains("Employer: LaunchCode"));
+            assertTrue(jobString.contains("Location: St. Louis"));
+            assertTrue(jobString.contains("Position Type: Front-end developer"));
+            assertTrue(jobString.contains("Competency: JavaScript"));
+            //chatGPT suggested using assertTrue, checking that the resulting string contains labels for the fields. passes, but fails when ran together
+        }
     }
